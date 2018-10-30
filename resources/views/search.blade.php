8712f40b0cf626e901c0624def08b917c967d65d
Robert Garza <robstudent1988@gmail.com>
2018-10-30 14:43:49 -0700

    add movie button now adds textarea for adding movie, can remove the area with a cancel button and submit. Submit is not implemented

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -33,8 +33,13 @@
 	</div>
 </main>
 
+
 <script type="text/javascript">
 	$('#searchButton').click(function() {
+		searchQuery();
+	});
+
+	function searchQuery() {
 		$("#appendSearch").empty();
 		var searchMovie = $('#search').val();
 		var requestString = "https://api.themoviedb.org/3/search/movie?api_key=547accc678865c1518f69b9f47e2d0a7&language=en-US&query=" + searchMovie + "&page=1";
@@ -48,11 +53,43 @@
 				var movieTitle = "<div class=\"card-header\"><div style=\"float:left;\"><b>" + currentMovie['title'] + "</b> (" + currentMovie['release_date'].substring(0,4) + ")</div> <div style=\"float:right;\"><b>TMDB average score:</b> " + currentMovie['vote_average'] + "</div><div class=\"clearfix\"></div></div><div class=\"card-body\"><div class=\"container\"><div class=\"row\">";
 				var posterUrl = "http://image.tmdb.org/t/p/w200" + currentMovie['poster_path'];
 				var posterImage = "<div class=\"col-sm-6 col-md-6 col-xs-12 image-container\"><img src=\"" + posterUrl + "\" alt=\"movie poster\"></div>";
-				var description =  "<div class=\"col-sm-6 col-md-6 col-xs-12\"><p class=\"card-text\"><b>Description:</b> " + currentMovie['overview'] + "</p><button class=\"btn btn-primary\">Add Movie</button></div>";
-				$("#appendSearch").append(makeCard + movieTitle + posterImage + description + "</div></div></div>");
+				var description =  "<div class=\"col-sm-6 col-md-6 col-xs-12\"><p class=\"card-text\"><b>Description:</b> " + currentMovie['overview'] + "</p><div id=\"div" + currentMovie['id'] + "\"></div>"
+				var buttonAdd = "<div id=\"theButtons" + currentMovie['id'] +"\"></div></div>";
+
+				$('#appendSearch').append(makeCard + movieTitle + posterImage + description + buttonAdd + "</div></div></div>");
+				visbleAddMovieButton(true, currentMovie['id']);
 			}
 		});
-	});
+	};
+
+	function visbleAddMovieButton(on, id){
+		$("#theButtons"+ id).empty();
+		var buttonText = "";
+		if (on) {
+			buttonText = "<button onclick=\"appendForm(" + id + ")\" class=\"btn btn-primary\">Add Movie</button>";
+			$("#theButtons"+id).append(buttonText);
+		} else {
+			buttonText = "<button onclick=\"cancelReview(" + id + ")\" class=\"btn btn-danger\">Cancel</button><button onclick=\"confirmReview(" + id + ")\" class=\"btn btn-primary\">Submit Review</button>";
+			$("#theButtons"+id).append(buttonText);
+		}
+	}
+
+	function appendForm(id) {
+		var movieReviewText = "<div class=\"form-group\"><label for=\"review\">Your Review:</label><textarea class=\"form-control\" id=\"exampleFormControlTextarea1\" rows=\"3\"></textarea></div>";
+		$('#div' + id).append(movieReviewText);
+		visbleAddMovieButton(false, id);
+	}
+
+	function cancelReview(id) {
+		$('#div' + id).empty();
+		visbleAddMovieButton(true, id);
+	}
+
+	/*Implement later: Submit Review */
+	function confirmReview(id){
+		console.log(id);
+	}
+
 </script>
 
 @endsection
