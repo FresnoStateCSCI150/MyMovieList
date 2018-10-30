8ed7f999846e94ff9673258e93a82bda4515abf7
Robert Garza <robstudent1988@gmail.com>
2018-10-30 12:45:19 -0700

    TMDB average score added to card headers on search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -39,17 +39,17 @@
 		var searchMovie = $('#search').val();
 		var requestString = "https://api.themoviedb.org/3/search/movie?api_key=547accc678865c1518f69b9f47e2d0a7&language=en-US&query=" + searchMovie + "&page=1";
 		$.get(requestString, function(data) {
-			console.log(data);
 			var obj = data['results'];
 			console.log(obj);
 			for (var i = 0; i < obj.length; i++) {
+				$("#appendSearch").append("<hr />");
 				var makeCard ="<div class=\"card\">";
 				var currentMovie = obj[i];
-				var movieTitle = "<div class=\"card-header\">" + currentMovie['title'] + "</div><div class=\"card-body\"><div class=\"container\"><div class=\"row\">";
+				var movieTitle = "<div class=\"card-header\"><div style=\"float:left;\"><b>" + currentMovie['title'] + "</b> (" + currentMovie['release_date'].substring(0,4) + ")</div> <div style=\"float:right;\"><b>TMDB average score:</b> " + currentMovie['vote_average'] + "</div><div class=\"clearfix\"></div></div><div class=\"card-body\"><div class=\"container\"><div class=\"row\">";
 				var posterUrl = "http://image.tmdb.org/t/p/w200" + currentMovie['poster_path'];
 				var posterImage = "<div class=\"col-sm-6 col-md-6 col-xs-12 image-container\"><img src=\"" + posterUrl + "\" alt=\"movie poster\"></div>";
-				var description =  "<div class=\"col-sm-6 col-md-6 col-xs-12\"><p class=\"card-text\"> " + currentMovie['overview'] + "</p></div>";
-				$("#appendSearch").append(makeCard + movieTitle + posterImage + description + "</div>");
+				var description =  "<div class=\"col-sm-6 col-md-6 col-xs-12\"><p class=\"card-text\"><b>Description:</b> " + currentMovie['overview'] + "</p><button class=\"btn btn-primary\">Add Movie</button></div>";
+				$("#appendSearch").append(makeCard + movieTitle + posterImage + description + "</div></div></div>");
 			}
 		});
 	});
