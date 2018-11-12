b041d9c1ac69f6d2b85350ef01d9710a33edde83
Jheovanny Camacho <jheocamacho@gmail.com>
2018-11-11 22:17:42 -0800

    Composer addition
    - added intervention/image for use in uploading images to account profile

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -209,6 +210,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
