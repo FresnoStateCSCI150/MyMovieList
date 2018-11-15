32d1f115bf69b1087c1436126f41e0031a8c56f7
Jheovanny Camacho <jheocamacho@gmail.com>
2018-11-15 12:08:05 -0800

    update home, about, account, navbar
    - homepage has two cards, for top 10 and recommended movies
    - added about page content
    - account/profile page has card to hold content
    - navbar adjusted to regular navbar instead of using tabs
    - added a logo

--- resources/views/_includes/nav/topnav.blade.php
+++ resources/views/_includes/nav/topnav.blade.php
@@ -1,22 +1,22 @@
 
 {{-- Style check --}}
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
 
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">My Movie List</a>
+      <a href="/" class="navbar-left"><img src="/logo.png" width="200px"></a>
     </div>
 
     {{-- if user is logged in, these become available --}}
     @if (Auth::check())
-    <ul class="nav nav-tabs navbar-left">
+    <ul class="navbar-nav navbar-left">
       <li class="nav-item"><a class="nav-link" href="/search">Search</a></li>
       <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
       <li class="nav-item"><a class="nav-link" href="/friends">Friends</a></li>
       <li class="nav-item"><a class="nav-link" href="/discussion">Discussions</a></li>
       <li class="nav-item"><a class="nav-link" href="https://github.com/FresnoStateCSCI150/MyMovieList">GitHub</a></li>
     </ul>
-    <ul class="nav nav-tabs navbar-left">
+    <ul class="navbar-nav navbar-left">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/account"> <img src="/uploads/avatars/{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:absolute; top:5px; left:-40px; border-radius:50%"> {{ Auth::user()->name }} </a>
         <div class="dropdown-menu">     
@@ -31,7 +31,7 @@
       </li>
     </ul>
     @else
-    <ul class="nav nav-tabs navbar-left">
+    <ul class="navbar-nav navbar-left">
       <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
       <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
       <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,35 @@
             <div class="top-right links">
                 @auth
                     <h1>Welcome, {{ Auth::user()->name }}!</h1>
+
+                    <hr>
+
+                    <div class="container">
+                        <div class="row justify-content-center">
+
+                            <div class="col-md">
+                                <div class="card">
+                                    <h4 class="card-header">{{ __('Your Top 10 Movies') }}</h4>
+                                    <div class="card-body">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md">
+                                <div class="card">
+                                    <h4 class="card-header">{{ __('Recommended Movies') }}</h4>
+                                    <div class="card-body">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
                 @else
-                    <h1>Welcome, to MyMovieList!</h1>
+                    <h1>Welcome to MyMovieList!</h1>
                     <h2>Please register or login!</h2>
                 @endauth
             </div>
