c205b0ce7ee4aef29f54b64fb2dacfdd7e0b39d5
Robert Garza <robstudent1988@gmail.com>
2018-11-15 11:57:24 -0800

    added more formating on tables

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,28 +7,29 @@
             <div class="top-right links">
                 @auth
                     <h1>Welcome, {{ Auth::user()->name }}!</h1>
+                    <div class="mx-auto" style="width: 200px;"><h2><u>Your Reviews</u></h2></div>
                     @foreach($reviews as $review)
                         <table class="table">
                             <thead>
                                 <tr>
+                                    <th scope="col">My Score</th>
                                     <th scope="col"></th>
                                     <th scope="col">Movie Title</th>
                                     <th scope="col">Movie Description</th>
                                     <th scope="col">Movie Release</th>
                                     <th scope="col">TMDB Score</th>
-                                    <th scope="col">My Score</th>
                                     <th scope="col">My Review</th>
                                 </tr>
                             </thead>
                             <tbody>
                             <tbody>
                                 <tr>
+                                    <td>{{$review->user_score}}</td>
                                     <td><img src="http://image.tmdb.org/t/p/w200{{$review->img_path}}"></td>
                                     <td>{{$review->title}}</td>
                                     <td>{{$review->description}}</td>
                                     <td>{{$review->release}}</td>
                                     <td>{{$review->tmdb_score}}</td>
-                                    <td>{{$review->user_score}}</td>
                                     <td>{{$review->review}}</td>
                                 </tr>
                             </tbody>
