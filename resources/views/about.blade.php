b38f50f3d04f3e315c2f878f136820c8862bdc89
Jheovanny Camacho <jheocamacho@gmail.com>
2018-11-24 17:28:28 -0800

    fixed about description
    - removed extra word in about page
    - temporary commented out code for layout of home page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
 
 	<p>MyMovieList is a social media website for movie lovers.</p>
 
-	<p>You can to search up movies, rate and review them, and also recommend them to other friends on the same site. Powered by TheMovieDatabase (TMDb) API.</p>
+	<p>You can search up movies, rate and review them, and also recommend them to other friends on the same site. Powered by TheMovieDatabase (TMDb) API.</p>
 
 	<p>Crafted with love by Jheovanny, Robert, Steven, and John.</p>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,6 +36,31 @@
                         </tbody>
                         </table>
 
+
+{{--                     <hr>
+
+                    <div class="container">
+                        <div class="row justify-content-center">
+
+                            <div class="col-md">
+                                <div class="card">
+                                    <h4 class="card-header">{{ __('Your Top 10 Movies') }}</h4>
+                                    <div class="card-body">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md">
+                                <div class="card">
+                                    <h4 class="card-header">{{ __('Recommended Movies') }}</h4>
+                                    <div class="card-body">
+
+                                    </div>
+                                </div>
+                            </div> --}}
+
+
                 @else
                     <h1>Welcome to MyMovieList!</h1>
                     <h2>Please register or login!</h2>
