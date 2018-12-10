b6953f54c12f82e10c6ea56f0088f37a352b7f8d
Jheovanny Camacho <jheocamacho@gmail.com>
2018-12-09 16:20:50 -0800

    Fixed Post model errors
    - fixed error from Post.php mentioning "no default values"
    - polished comments section to look more like a message board
    - moved about link to the end of navbar (least important link)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,32 +4,65 @@
 
 	<div class="col-sm-8">
 
-		<h1>{{ $post->title }}</h1>
+		{{-- original poster --}}
+		<div class='container'>
+    		<div class='row mb-3'>
+        		<div class='col-md'>
+            		<div class='card shadow-sm bg-white rounded'>
+            			<div class='card-header'>
+            				<h1>{{ $post->title }}</h1>
+            				<hr>
+            				<div class='row'>
+            					<div class='col'>
+            						<h6><strong>{{ $post->user->name }}</strong></h6>
+            					</div>
+            					<div class="w-200"></div>
+            					<div class='col'>
+            						<h6 class="text-right"><strong>{{ $post->created_at->diffForHumans() }}</strong> &nbsp; {{ $post->created_at->tz('America/Los_Angeles')->toDayDateTimeString() }}</h6>
+            					</div>
+            				</div>
 
-		{{ $post->body }}
-
-		<hr>
- 
-		{{-- List of available comments --}}
-		@if(count($post->comments))	
-		<div class="comments">
-			<ul class="list-group">
-				@foreach ($post->comments as $comment)
-					<li class="list-group-item">
-						<strong>
-							{{ $comment->created_at->diffForHumans() }}: &nbsp;
-						</strong>
-						{{ $comment->body }}
-					</li>
-				@endforeach
-			</ul>
+            			</div>
+                		<div class='card-body'>
+							{{ $post->body }}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
-		<hr>
+		{{-- List of available comments --}}
+		@if(count($post->comments))
+		@foreach ($post->comments as $comment)
+		<div class='container'>
+    		<div class='row mb-3'>
+        		<div class='col-md'>
+            		<div class='card shadow-sm bg-white rounded'> 
+            			<div class='card-header'>
+            				<div class='row'>
+            					<div class='col'>
+            						<h6><strong>{{ $comment->user->name }}</strong></h6>
+            					</div>
+            					<div class="w-200"></div>
+            					<div class='col'>
+            						<h6 class="text-right"><strong>{{ $comment->created_at->diffForHumans() }}</strong> &nbsp; {{ $comment->created_at->tz('America/Los_Angeles')->toDayDateTimeString() }}</h6>
+            					</div>
+            				</div>
+            			</div>            			
+                		<div class='card-body'>
+							{{ $comment->body }}
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		@endforeach
 		@endif
 
+		<hr>
+
 		{{-- Add a comment --}}
-		<div class="card">
+		<div class="card shadow-sm bg-white rounded mb-3">
 			<div class="card-block m-3">
 				<h5>Join the conversation.</h5>
 				<form method="POST" action="/discussion/{{ $post->id }}/comments">
